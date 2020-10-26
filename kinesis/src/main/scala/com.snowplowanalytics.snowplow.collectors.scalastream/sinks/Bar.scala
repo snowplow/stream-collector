5f6e565c9f7082dca2baf639e0fc83683c95d2de
Łukasz Indykiewicz <lukasz.indykiewicz@gmail.com>
2020-10-26 23:21:52 +0100

    Add grouping max timeout

--- kinesis/src/main/scala/com.snowplowanalytics.snowplow.collectors.scalastream/sinks/Bar.scala
+++ kinesis/src/main/scala/com.snowplowanalytics.snowplow.collectors.scalastream/sinks/Bar.scala
@@ -8,7 +8,11 @@ import scala.util.control.NonFatal
 
 object Bar extends App {
 
-  case class IntWithCounter(x: Int, counter: Int)
+  case class IntWithCounter(
+    x: Int,
+    counter: Int,
+    nanoTime: Long
+  )
 
   val q = new ConcurrentLinkedQueue[IntWithCounter]
   val executorService = new ScheduledThreadPoolExecutor(4)
@@ -19,7 +23,7 @@ object Bar extends App {
   val kinesisBuffer = new ConcurrentLinkedQueue[IntWithCounter]
   val sqsBuffer = new ConcurrentLinkedQueue[IntWithCounter]
 
-  q.addAll((1 to 100).toList.map(x => (IntWithCounter(x, 3))).asJava)
+  q.addAll((1 to 100).toList.map(x => (IntWithCounter(x, 3, 0L))).asJava)
 
   scala.sys.addShutdownHook {
     println(s"shutting down with shutdown hook")
@@ -64,25 +68,31 @@ object Bar extends App {
     from: ConcurrentLinkedQueue[IntWithCounter],
     to: ConcurrentLinkedQueue[IntWithCounter],
     max: Int,
+    maxDuration: FiniteDuration,
     func: () => Unit
-  ) =
-    if (from.peek() != null && to.size < max) {
-      to.add(from.poll)
-      ()
-    } else if (to.size >= max) {
+  ) = {
+    val peekNano = Option(to.peek).map(_.nanoTime).getOrElse(System.nanoTime)
+    val overMax = System.nanoTime > peekNano + maxDuration.toNanos
+    if (to.size >= max || overMax) {
       func()
+    } else if (from.peek() != null && to.size < max) {
+      to.add(from.poll.copy(nanoTime = System.nanoTime))
+      ()
     }
+  }
 
   def bufferedKinesis(): Unit =
     buffer(
       kinesisQ,
       kinesisBuffer,
       20,
+      10.second,
       kinesisFlow
     )
 
   def kinesisFlow(): Unit = {
-    val retryToKinesis = (1 to 20).toList.map(_ => Option(kinesisBuffer.poll)).flatten
+    val retryToKinesis =
+      (1 to kinesisBuffer.size).toList.map(_ => Option(kinesisBuffer.poll)).flatten
     if (retryToKinesis.nonEmpty) {
       println(s"Sending to Kinesis: $retryToKinesis")
       q.addAll(retryToKinesis.map(ev => ev.copy(counter = ev.counter - 1)).asJava)
@@ -95,11 +105,12 @@ object Bar extends App {
       sqsQ,
       sqsBuffer,
       10,
+      10.second,
       sqsFlow
     )
 
   def sqsFlow(): Unit = {
-    val toSqs = (1 to 10).toList.map(_ => Option(sqsBuffer.poll)).flatten
+    val toSqs = (1 to sqsBuffer.size).toList.map(_ => Option(sqsBuffer.poll)).flatten
     if (toSqs.nonEmpty) println(s"Sending to SQS: $toSqs")
   }
 
