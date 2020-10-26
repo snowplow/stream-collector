9aba29566e0920131da971e1dbb87684994f7212
Łukasz Indykiewicz <lukasz.indykiewicz@gmail.com>
2020-10-26 22:33:21 +0100

    Add different grouping to the example

--- kinesis/src/main/scala/com.snowplowanalytics.snowplow.collectors.scalastream/sinks/Bar.scala
+++ kinesis/src/main/scala/com.snowplowanalytics.snowplow.collectors.scalastream/sinks/Bar.scala
@@ -13,8 +13,13 @@ object Bar extends App {
   val q = new ConcurrentLinkedQueue[IntWithCounter]
   val executorService = new ScheduledThreadPoolExecutor(4)
 
+  val kinesisQ = new ConcurrentLinkedQueue[IntWithCounter]
+  val sqsQ = new ConcurrentLinkedQueue[IntWithCounter]
+
+  val kinesisBuffer = new ConcurrentLinkedQueue[IntWithCounter]
+  val sqsBuffer = new ConcurrentLinkedQueue[IntWithCounter]
+
   q.addAll((1 to 100).toList.map(x => (IntWithCounter(x, 3))).asJava)
-  println(s"q: $q")
 
   scala.sys.addShutdownHook {
     println(s"shutting down with shutdown hook")
@@ -22,31 +27,80 @@ object Bar extends App {
   }
 
   executorService.scheduleAtFixedRate(
-    () => logNonFatalAndKeepRunning(),
+    () => logNonFatalAndKeepRunning(bar),
     100,
     10,
     MILLISECONDS
   )
 
-  def logNonFatalAndKeepRunning() =
+  executorService.scheduleAtFixedRate(
+    () => logNonFatalAndKeepRunning(bufferedKinesis),
+    500,
+    10,
+    MILLISECONDS
+  )
+
+  executorService.scheduleAtFixedRate(
+    () => logNonFatalAndKeepRunning(bufferedSqs),
+    600,
+    10,
+    MILLISECONDS
+  )
+
+  def logNonFatalAndKeepRunning(func: () => Unit): Unit =
     try {
-      bar()
+      func()
     } catch {
       case NonFatal(e) =>
         println(s"ERROR: ${e.getMessage}")
     }
 
-  def bar() = {
-    val xs = (1 to 20).toList.map(_ => Option(q.poll)).flatten
-    val (retryToKinesis, toSqs) = xs.partition(_.counter > 0)
+  def bar() =
+    Option(q.poll()).foreach { head =>
+      if (head.counter > 0) kinesisQ.add(head) else sqsQ.add(head)
+    }
 
+  def buffer(
+    from: ConcurrentLinkedQueue[IntWithCounter],
+    to: ConcurrentLinkedQueue[IntWithCounter],
+    max: Int,
+    func: () => Unit
+  ) =
+    if (from.peek() != null && to.size < max) {
+      to.add(from.poll)
+      ()
+    } else if (to.size >= max) {
+      func()
+    }
+
+  def bufferedKinesis(): Unit =
+    buffer(
+      kinesisQ,
+      kinesisBuffer,
+      20,
+      kinesisFlow
+    )
+
+  def kinesisFlow(): Unit = {
+    val retryToKinesis = (1 to 20).toList.map(_ => Option(kinesisBuffer.poll)).flatten
     if (retryToKinesis.nonEmpty) {
       println(s"Sending to Kinesis: $retryToKinesis")
       q.addAll(retryToKinesis.map(ev => ev.copy(counter = ev.counter - 1)).asJava)
     }
+    ()
+  }
 
+  def bufferedSqs() =
+    buffer(
+      sqsQ,
+      sqsBuffer,
+      10,
+      sqsFlow
+    )
+
+  def sqsFlow(): Unit = {
+    val toSqs = (1 to 10).toList.map(_ => Option(sqsBuffer.poll)).flatten
     if (toSqs.nonEmpty) println(s"Sending to SQS: $toSqs")
-
   }
 
 }
