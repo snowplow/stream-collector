3bf4a87b2787215d384b109ba7f7665f6e22ca21
Dilyan Damyanov <dilyand@gmail.com>
2021-12-13 13:02:32 +0000

    Bump joda-time to 2.10.13 (close #198)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -28,7 +28,7 @@ object Dependencies {
     val pubsub       = "1.113.7"
     val kafka        = "2.2.1"
     val nsqClient    = "1.3.0"
-    val jodaTime     = "2.10.8"
+    val jodaTime     = "2.10.13"
     val slf4j        = "1.7.32"
     val log4j        = "2.16.0" // CVE-2021-44228
     val config       = "1.4.1"
