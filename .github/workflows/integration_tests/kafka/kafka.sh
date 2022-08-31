96bef654edff538242f2ce61608cc48cf0101a16
TiganeteaRobert <roberttiganetea@yahoo.com>
2022-08-31 14:55:48 +0300

    with bash script

--- .github/workflows/integration_tests/kafka/kafka.sh
+++ .github/workflows/integration_tests/kafka/kafka.sh
@@ -6,20 +6,19 @@ sleep 10
 
 echo "waiting for kafka to be ready..."
 
-docker exec broker cub kafka-ready -b broker:9092 1 120
+sleep 5
 
-echo "kafka is ready, creating topics..."
-
-docker exec broker kafka-topics --bootstrap-server broker:9092 --create --topic good && \
-docker exec broker kafka-topics --bootstrap-server broker:9092 --create --topic bad
-
-echo "topics created, running collector..."
+echo "running collector..."
 
 docker run --network="integration_net1" --name collector -d -v "$PWD"/.github/workflows/kafka-collector-config:/snowplow/config -p 12345:12345 snowplow/scala-stream-collector-kafka:0.0.0 --config /snowplow/config/config.hocon
 
 echo "waiting for collector to start..."
-sleep 30
+sleep 5
 
+docker logs collector
+
+docker network ls
+docker network inspect integration_net1
 docker ps
 
 echo "sending event to collector..."
@@ -28,7 +27,8 @@ curl -X POST -i http://0.0.0.0:12345/com.snowplowanalytics.snowplow/tp2  -d 'tes
 docker logs collector
 
 echo "consuming from kafka broker, good topic"
-timeout 30 docker exec -w /bin broker "kafka-console-consumer" --topic=good --from-beginning --bootstrap-server 0.0.0.0:9092 | grep -q "test"
+timeout 5 docker exec -w /bin broker kafka-console-consumer.sh --topic=good --from-beginning --bootstrap-server 0.0.0.0:9092 | grep -q "test"
 
 echo "stopping docker containers"
 docker stop $(docker ps -aq)
+
