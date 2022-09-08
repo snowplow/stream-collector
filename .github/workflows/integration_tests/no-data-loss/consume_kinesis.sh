2dae2921f66fedee686dac7da6b695d9150c9279
TiganeteaRobert <roberttiganetea@yahoo.com>
2022-09-08 20:58:41 +0300

    wip, working kinesis connection + scripts

--- .github/workflows/integration_tests/no-data-loss/consume_kinesis.sh
+++ .github/workflows/integration_tests/no-data-loss/consume_kinesis.sh
@@ -5,18 +5,27 @@ streamName=$1
 export PYTHONWARNINGS="ignore:Unverified HTTPS request"
 
 shard=$(aws --endpoint-url=http://localhost:4566 kinesis describe-stream --stream-name $streamName --output text | grep SHARDS | awk '{print $2}')
-iterator=$(aws --endpoint-url=http://localhost:4566 kinesis get-shard-iterator --stream-name $streamName --shard-id $shard --shard-iterator-type LATEST --output text)
+iterator=$(aws --endpoint-url=http://localhost:4566 kinesis get-shard-iterator --stream-name $streamName --shard-id $shard --shard-iterator-type TRIM_HORIZON --output text)
+lines=$(aws --endpoint-url=http://localhost:4566 kinesis get-records --shard-iterator $iterator --output text | tail -n +2|awk '{print $3}' & sleep 1; kill $!)
 
-while true
-do
-  lines=$(aws --endpoint-url=http://localhost:4566 kinesis get-records --shard-iterator $iterator --output text|tail -n +2|awk '{print $3}')
+while read p; do
+  found=false
+  echo "$p"
   for l in $lines
   do
-    echo $l|base64 -d
-    echo -e "\n"
+    a=$(echo "$l" | base64 -d)
+    if [[ "$a" == *"$p"* ]]; then
+      found=true
+      break
+    fi
   done
-  if [[ -n $lines ]]
+  if [ $found = true ]
   then
-    iterator=$(aws --endpoint-url=http://localhost:4566 kinesis get-shard-iterator --stream-name $streamName --shard-id $shard --shard-iterator-type LATEST --output text)
+      continue
   fi
-done
+  break
+done <"$PWD"/.github/workflows/integration_tests/no-data-loss/data.txt
+
+echo "Found all records in Kinesis"
+
+
