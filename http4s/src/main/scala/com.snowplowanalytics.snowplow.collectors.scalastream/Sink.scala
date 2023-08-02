4f06054396641942514cb23f328dec61330fcaaa
Ian Streeter <ian@snowplowanalytics.com>
2023-08-02 21:13:31 +0100

    http4s graceful shutdown

---
+++ http4s/src/main/scala/com.snowplowanalytics.snowplow.collectors.scalastream/Sink.scala
@@ -0,0 +1,11 @@
+package com.snowplowanalytics.snowplow.collectors.scalastream
+
+trait Sink[F[_]] {
+
+  // Maximum number of bytes that a single record can contain.
+  // If a record is bigger, a size violation bad row is emitted instead
+  val maxBytes: Int
+
+  def isHealthy: F[Boolean]
+  def storeRawEvents(events: List[Array[Byte]], key: String): F[Unit]
+}
