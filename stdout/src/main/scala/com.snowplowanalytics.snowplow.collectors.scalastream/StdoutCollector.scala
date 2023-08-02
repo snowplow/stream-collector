a10d6bc62a56d05a880c31cbc6229ad58d499b26
Ian Streeter <ian@snowplowanalytics.com>
2023-08-02 21:13:31 +0100

    Add http4s graceful shutdown (close #365)

--- stdout/src/main/scala/com.snowplowanalytics.snowplow.collectors.scalastream/StdoutCollector.scala
+++ stdout/src/main/scala/com.snowplowanalytics.snowplow.collectors.scalastream/StdoutCollector.scala
@@ -8,10 +8,32 @@
  */
 package com.snowplowanalytics.snowplow.collectors.scalastream
 
-import cats.effect.{ExitCode, IO, IOApp}
+import cats.effect.{ExitCode, IO, IOApp, Sync}
+import cats.effect.kernel.Resource
+import cats.implicits._
+
+import java.util.Base64
+import java.io.PrintStream
 
 object StdoutCollector extends IOApp {
 
-  def run(args: List[String]): IO[ExitCode] =
-    CollectorApp.run()
+  def run(args: List[String]): IO[ExitCode] = {
+    val good = Resource.pure[IO, Sink[IO]](printingSink(System.out))
+    val bad  = Resource.pure[IO, Sink[IO]](printingSink(System.err))
+    CollectorApp.run[IO](good, bad)
+  }
+
+  private def printingSink[F[_]: Sync](stream: PrintStream): Sink[F] = new Sink[F] {
+    val maxBytes              = Int.MaxValue // TODO: configurable?
+    def isHealthy: F[Boolean] = Sync[F].pure(true)
+
+    val encoder = Base64.getEncoder().withoutPadding()
+
+    def storeRawEvents(events: List[Array[Byte]], key: String): F[Unit] =
+      events.traverse_ { e =>
+        Sync[F].delay {
+          stream.println(encoder.encodeToString(e))
+        }
+      }
+  }
 }
