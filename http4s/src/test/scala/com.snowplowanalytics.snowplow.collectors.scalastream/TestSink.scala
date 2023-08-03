d9da0e7d89bf0fb7ad04e464119df67d972a0bf4
spenes <enes@snowplowanalytics.com>
2023-08-03 13:32:15 +0300

    Add http4s POST endpoint (close #366)

---
+++ http4s/src/test/scala/com.snowplowanalytics.snowplow.collectors.scalastream/TestSink.scala
@@ -0,0 +1,20 @@
+package com.snowplowanalytics.snowplow.collectors.scalastream
+
+import cats.effect.IO
+
+import scala.collection.mutable.ListBuffer
+
+class TestSink extends Sink[IO] {
+
+  private val buf: ListBuffer[Array[Byte]] = ListBuffer()
+
+  override val maxBytes: Int = Int.MaxValue
+
+  override def isHealthy: IO[Boolean] = IO.pure(true)
+
+  override def storeRawEvents(events: List[Array[Byte]], key: String): IO[Unit] =
+    IO.delay(buf ++= events)
+
+  def storedRawEvents: List[Array[Byte]] = buf.toList
+
+}
