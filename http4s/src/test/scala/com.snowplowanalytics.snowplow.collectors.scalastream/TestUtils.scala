c89dd86feedcfd03ab6b69f68a25e8419e3b1582
spenes <enes@snowplowanalytics.com>
2023-08-07 15:26:50 +0300

    Configure set-cookie header (close #368)

--- http4s/src/test/scala/com.snowplowanalytics.snowplow.collectors.scalastream/TestUtils.scala
+++ http4s/src/test/scala/com.snowplowanalytics.snowplow.collectors.scalastream/TestUtils.scala
@@ -1,6 +1,7 @@
 package com.snowplowanalytics.snowplow.collectors.scalastream
 
-import com.snowplowanalytics.snowplow.collectors.scalastream.model.CollectorConfig
+import scala.concurrent.duration._
+import com.snowplowanalytics.snowplow.collectors.scalastream.model._
 
 object TestUtils {
 
@@ -9,6 +10,16 @@ object TestUtils {
       "/com.acme/track"    -> "/com.snowplowanalytics.snowplow/tp2",
       "/com.acme/redirect" -> "/r/tp2",
       "/com.acme/iglu"     -> "/com.snowplowanalytics.iglu/v1"
+    ),
+    cookie = CookieConfig(
+      enabled        = true,
+      name           = "sp",
+      expiration     = 365.days,
+      domains        = List.empty,
+      fallbackDomain = None,
+      secure         = false,
+      httpOnly       = false,
+      sameSite       = None
     )
   )
 }
