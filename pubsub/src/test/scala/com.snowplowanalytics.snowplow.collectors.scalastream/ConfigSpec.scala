66f58bcb14336f362997ceed8cb0422061281836
Piotr Limanowski <peel@users.noreply.github.com>
2023-09-18 19:44:46 +0200

    Make maxConnections and idleTimeout configurable (close #386)
    Previously, idleTimeout has been hardcoded, maxConnections hasn't been
    configured.
    Now, these parameters are set within `networking` section and used throughout
    http4s backends.

--- pubsub/src/test/scala/com.snowplowanalytics.snowplow.collectors.scalastream/ConfigSpec.scala
+++ pubsub/src/test/scala/com.snowplowanalytics.snowplow.collectors.scalastream/ConfigSpec.scala
@@ -95,6 +95,10 @@ object ConfigSpec {
     enableDefaultRedirect = false,
     redirectDomains       = Set.empty,
     preTerminationPeriod  = 10.seconds,
+    networking = Config.Networking(
+      maxConnections = 1024,
+      idleTimeout    = 610.seconds
+    ),
     streams = Config.Streams(
       good                       = "good",
       bad                        = "bad",
