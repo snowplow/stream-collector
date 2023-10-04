3711dcaa95e2c886d6e76377ac8fa163e4c6069e
spenes <enes@snowplowanalytics.com>
2023-10-04 15:29:23 +0300

    Set installation id (close #384)

---
+++ sqs/src/main/scala/com.snowplowanalytics.snowplow.collectors.scalastream/TelemetryUtils.scala
@@ -0,0 +1,25 @@
+package com.snowplowanalytics.snowplow.collectors.scalastream
+
+import cats.effect.{IO, Resource}
+import com.snowplowanalytics.snowplow.collector.core.Config
+import com.snowplowanalytics.snowplow.collectors.scalastream.sinks._
+
+object TelemetryUtils {
+
+  def getAccountId(config: Config.Streams[SqsSinkConfig]): IO[Option[String]] =
+    Resource
+      .make(
+        IO(SqsSink.createSqsClient(config.sink.region)).rethrow
+      )(c => IO(c.shutdown()))
+      .use { client =>
+        IO {
+          val sqsQueueUrl = client.getQueueUrl(config.good).getQueueUrl
+          Some(extractAccountId(sqsQueueUrl))
+        }
+      }
+      .handleError(_ => None)
+
+  def extractAccountId(sqsQueueUrl: String): String =
+    sqsQueueUrl.split("/")(3)
+
+}
