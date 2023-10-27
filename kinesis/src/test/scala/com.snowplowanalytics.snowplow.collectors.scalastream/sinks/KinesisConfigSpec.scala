c4795f5a07b09a7a44cd9415779af936c3b96808
Piotr Limanowski <peel@users.noreply.github.com>
2023-10-27 16:17:27 +0200

    Add Snowplow Community License (close #346)

--- kinesis/src/test/scala/com.snowplowanalytics.snowplow.collectors.scalastream/sinks/KinesisConfigSpec.scala
+++ kinesis/src/test/scala/com.snowplowanalytics.snowplow.collectors.scalastream/sinks/KinesisConfigSpec.scala
@@ -6,7 +6,7 @@
   * and you may not use this file except in compliance with the Snowplow Community License Version 1.0.
   * You may obtain a copy of the Snowplow Community License Version 1.0 at https://docs.snowplow.io/community-license-1.0
   */
-package com.snowplowanalytics.snowplow.collectors.scalastream.sinks
+package com.snowplowanalytics.snowplow.collectors.scalastream
 
 import cats.effect.testing.specs2.CatsEffect
 import cats.effect.{ExitCode, IO}
